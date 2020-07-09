2eba6b52c3b72489ed21b43c97dff99d7eb792d4
markusritschel <git@markusritschel.de>
2020-07-09 23:22:55 +0200

    Add CeraQuery class
    The Cera search routine returns an instance of the CeraQuery class. The representation of this instance is the number of unique entries per variable in the search result.
    The class provides a `df` attribute, which represents the search result in form of a pd.DataFrame, with the columns being the variables and the according assets (usually the path to a netCDF file) from the search result.

--- dkrz_cera/cera_access.py
+++ dkrz_cera/cera_access.py
@@ -9,6 +9,8 @@
 #
 import os
 import re
+import shutil
+import pandas as pd
 import requests
 
 
@@ -116,12 +118,142 @@ class Cera(object):
             if start > entries_found:
                 break
 
+        query_obj = CeraQuery(self, results_dict)
         print("{} results found\n".format(len(results_dict)))
 
-        return results_dict
+        return query_obj
+
+
+class CeraQuery(object):
+    """This returns an instance similar to the intake-esm collection class. It provides a search() method as  well as an
+    interfaces to Pandas to view the results as a pd.DataFrame"""
+
+    def __init__(self, parent, results_dict):
+        """Takes dictionary with lists as values as well as a reference to the parent object (an instance of Cera)"""
+        self.parent = parent
+        self.__results_dict = results_dict
+
+    def __str__(self):
+        self.__repr__()
+        return ''
+
+    def __repr__(self):
+        from IPython.display import display
+        display(pd.DataFrame(self.df.nunique(), columns=['nunique']))
+        return ''
+
+    def to_jblob(self, download_path, file=None, jblob_log_dir='/tmp/'):
+        """This provides a method to autogenerate shell scripts, which can be used for downloading data from DKRZ's CERA
+        database via Jblob."""
+        self.parent.check_access()
+
+        if not file:
+            file = os.path.expanduser('~/jblob_cera_download.sh')
+        else:
+            file = os.path.expanduser(file)
+
+        download_path = os.path.expanduser(download_path)
+        os.makedirs(download_path, exist_ok=True)
+
+        jblob_path = shutil.which('jblob')
+        if jblob_path:
+            jblob_install_dir = os.path.dirname(jblob_path)
+            jblob_log_dir = os.path.join(jblob_install_dir, 'logs')
+            os.makedirs(jblob_log_dir, exist_ok=True)
+        else:
+            jblob_install_dir = ''
+            print(
+                "\nCouldn't find jblob. Make sure jblob is installed and change the JBLOB_HOME value in {} accordingly.".format(
+                    file))
+
+        jblob_log_file = os.path.join(jblob_log_dir, '$0.log')
+
+        with open(file, 'w') as jblob_file:
+            jblob_file.write(
+                """#!/bin/bash
+                
+                # This script requires a local Jblob installation. Jblob is a java
+                # based tool for data download from the CERA database available from
+                # https://cera-www.dkrz.de/WDCC/ui/cerasearch/info?site=jblob
+                
+                # If you experience problems when using this script or have questions please
+                # contact data@dkrz.de.
+                
+                # Please prepare your .netrc file to contain the necessary entry for
+                # CERA database access, for more information see "Supplying username and
+                # password" at https://cera-www.dkrz.de/WDCC/ui/cerasearch/info?site=jblob
+                
+                # Edit JBLOB_HOME to match your Jblob installation directory if the
+                # 'jblob' executable cannot be found in $PATH.
+                JBLOB_HOME={0:}
+                
+                # Locate jblob executable
+                if [[ -z $JBLOB_HOME ]]; then
+                JBLOB=$(which jblob 2> /dev/null)
+                RC=$?
+                if [[ $RC -ne 0 ]]; then
+                    echo "Cannot locate jblob executable, please edit JBLOB_HOME and re-run this script!"
+                    exit 1
+                fi
+                else
+                JBLOB=$JBLOB_HOME/jblob
+                fi
+                
+                # Logfile
+                LOG={1:}\n\n""".format(jblob_install_dir, jblob_log_file))
+
+            for entry_name, entry_acronym in zip(self.df['entry_name_s'], self.df['entry_acronym_s']):
+                dataset_dir = os.path.join(download_path, *entry_name.split(' '))
+                jblob_file.write('mkdir -p {}\n'.format(dataset_dir))
+                jblob_file.write('$JBLOB --dataset {} --dir {}\n'.format(entry_acronym, dataset_dir))
+                jblob_file.write('echo "{}: $?" >> $LOG\n'.format(entry_acronym))
+
+            jblob_file.write('\necho "' + '-'*40 + '" >> $LOG')
+            jblob_file.write('\necho "All done..." >> $LOG')
+            jblob_file.write('\necho `/bin/date "+%Y-%m-%d %H:%M:%S "` >> $LOG')
+            jblob_file.write('\necho "' + '='*40 + '" >> $LOG\n')
+
+            print('\njblob file "{1:}" successfully created in {0:}\n'.format(*os.path.split(jblob_file.name)))
+
+            print("For downloading data from the CERA database, run the created jblob file in the shell.\n"
+                  "Since the download might take a while, it is recommended to start a `screen` environment to run it in the background.\n"
+                  "With `screen` one can start a virtual terminal environment. After starting the file in there, you can easily\n"
+                  "detach yourself from the environment by pressing `Ctrl+A, D`. You will notice a '[detached]' message in the terminal.\n"
+                  "To return to the environment simply type `screen -r` or first list your running screen environments with `screen -list` \n"
+                  "if you have multiple running.\n"
+                  "Check `man screen` for further information.\n")
+
+    @property
+    def df(self):
+        """Returns a pd.DataFrame of"""
+        df_dict = dict()
+        for key, entry in self.__results_dict.items():
+            values = [entry.entry_acronym_s,
+                      entry.model_s,
+                      entry.qc_experiment_s,
+                      entry.variable_s,
+                      entry.frequency_s,
+                      entry.entry_name_s,
+                      entry.project_acronym_ss[0],
+                      entry.format_acronym_s]
+            df_dict[key] = values
+
+        columns = ['entry_acronym_s',
+                   'model_s',
+                   'qc_experiment_s',
+                   'variable_s',
+                   'frequency_s',
+                   'entry_name_s',
+                   'project_acronym_ss',
+                   'format_acronym_s']
+        df = pd.DataFrame.from_dict(df_dict, orient='index', columns=columns)
+
+        return df
 
 
 if __name__=='__main__':
     cera = Cera()
+    # SEARCH_PATTERN = dict(q="cmip5*historicalNat *mon*tas")
     cs = cera.search(variable_s='tas', model_s='ACCESS1-0', qc_experiment_s='historical')
+    # cs.to_jblob('~/work/cmip5_download')
     print(cs)

4d35703474c164aafef1313349ffa5bdc1ec5f41
markusritschel <git@markusritschel.de>
2020-07-09 22:48:11 +0200

    setup new project from cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dkrz-cera',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Provides an interface to the CERA database of the DKRZ (Deutsches Klimarechenzentrum)',
+    author='Markus Ritschel',
+    license='MIT',
+)

bfb9f72681e80eaeaaf3acc154fec753859c3155
Markus Ritschel <git@markusritschel.de>
2026-05-08 17:53:33 +0200

    feat: support CERA credentials from env vars and .env file
    Extends Cera.__get_credentials() to check three sources in priority order:
    1. CERA_USER + CERA_PASSWORD (or CERA_PASS) environment variables
    2. .env file in cwd then home directory, parsed via python-dotenv
    3. ~/.netrc (existing fallback, unchanged)
    
    Adds unit tests covering each credential source using monkeypatch/tmp_path.

--- src/dkrz_cera/cera_access.py
+++ src/dkrz_cera/cera_access.py
@@ -13,19 +13,19 @@ import shutil
 
 import pandas as pd
 import requests
-
+from dotenv import dotenv_values
 
 __all__ = ["Cera", "CeraQuery"]
 
 
-class JSONObject(object):
+class JSONObject:
     """A helper class to access the JSON elements from the CERA request like attributes"""
 
     def __init__(self, d):
         self.__dict__ = d
 
 
-class Cera(object):
+class Cera:
     def __init__(self):
         self.login_url = "https://cera-www.dkrz.de/WDCC/ui/cerasearch/login"
         self.__username = None
@@ -36,21 +36,59 @@ class Cera(object):
         self.__get_credentials()
         # TODO: checking the actual access is not possible at the moment
 
-    def __get_credentials(self, netrc_file='.netrc'):
+    def __get_credentials(self, netrc_file=".netrc"):
+        # Resolution order: env vars → .env file (cwd then home) → ~/.netrc
+
+        # 1. Environment variables
+        user = os.environ.get("CERA_USER")
+        password = os.environ.get("CERA_PASSWORD") or os.environ.get("CERA_PASS")
+        if user and password:
+            self.__username = user
+            self.__password = password
+            print(
+                f"CERA credentials found (user: {self.__username}) via environment variables"
+            )
+            return
+
+        # 2. .env file in cwd, then home directory
+        for search_dir in [os.getcwd(), os.path.expanduser("~")]:
+            dotenv_path = os.path.join(search_dir, ".env")
+            if os.path.isfile(dotenv_path):
+                env_values = dotenv_values(dotenv_path)
+                user = env_values.get("CERA_USER")
+                password = env_values.get("CERA_PASSWORD") or env_values.get(
+                    "CERA_PASS"
+                )
+                if user and password:
+                    self.__username = user
+                    self.__password = password
+                    print(
+                        f"CERA credentials found (user: {self.__username}) via {dotenv_path}"
+                    )
+                    return
+
+        # 3. ~/.netrc fallback
         try:
-            with open(os.path.join(os.getenv('HOME'), netrc_file)) as f:
+            netrc_path = os.path.join(os.path.expanduser("~"), netrc_file)
+            with open(netrc_path) as f:
                 x = re.findall(r"login\s+(\w+)\s+password\s+(\w+)", f.read())
-                if len(x)==1:
+                if len(x) == 1:
                     self.__username, self.__password = x[0]
-                    print("CERA credentials (user: {}) found in {}".format(self.__username, f.name))
+                    print(
+                        f"CERA credentials found (user: {self.__username}) via {f.name}"
+                    )
                 elif len(x) > 1:
-                    print("More than one pair of username and password found. Please check your {}.".format(f.name))
+                    print(
+                        f"More than one pair of username and password found. Please check your {f.name}."
+                    )
                 else:
-                    print("Cannot find user credentials in {}".format(f.name))
-        except:
-            print("Cannot find user credentials. Make sure you have a .netrc file in your home directory "
-                  "matching the requirements under \n'Supplying username and password' "
-                  "at https://cera-www.dkrz.de/WDCC/ui/cerasearch/info?site=jblob")
+                    print(f"Cannot find user credentials in {f.name}")
+        except FileNotFoundError:
+            print(
+                "Cannot find user credentials. Make sure you have a .netrc file in your home directory "
+                "matching the requirements under \n'Supplying username and password' "
+                "at https://cera-www.dkrz.de/WDCC/ui/cerasearch/info?site=jblob"
+            )
 
     def search(self, **query):
         """Performs a search in the CERA database.
@@ -71,29 +109,35 @@ class Cera(object):
         -------
         query_obj : CeraQuery
         """
-        valid_kwargs = ['variable_s',
-                        'frequency_s',
-                        'entry_name_s',
-                        'model_s',
-                        'qc_experiment_s',
-                        'format_acronym_s',
-                        'project_acronym_s']
+        valid_kwargs = [
+            "variable_s",
+            "frequency_s",
+            "entry_name_s",
+            "model_s",
+            "qc_experiment_s",
+            "format_acronym_s",
+            "project_acronym_s",
+        ]
 
         for key, val in query.items():
             if key not in valid_kwargs:
-                print(f"{key} is not a valid search key. Search keys must be one of {', '.join(valid_kwargs)}")
+                print(
+                    f"{key} is not a valid search key. Search keys must be one of {', '.join(valid_kwargs)}"
+                )
             if isinstance(val, (list, tuple)):
                 query[key] = f"+OR+{key}:".join(val)
 
         # Example: "model_s:bcc-*+variable_s:tas+qc_experiment_s:historicalGHG+OR+qc_experiment_s:historicalNat"
-        query_str = '+'.join(['{}:{}'.format(k, v) for k, v in query.items()])
+        query_str = "+".join([f"{k}:{v}" for k, v in query.items()])
 
         rows = 100
         start = 0
         results_dict = dict()
         while True:
-            url2scrape = 'https://cera-www.dkrz.de/WDCC/ui/cerasearch/solr/select?wt=json&facet.mincount=1&' \
-                         'q={}&start={}&rows={}'.format(query_str, start, rows)
+            url2scrape = (
+                "https://cera-www.dkrz.de/WDCC/ui/cerasearch/solr/select?wt=json&facet.mincount=1&"
+                f"q={query_str}&start={start}&rows={rows}"
+            )
             r = requests.get(url2scrape)
             json_dict = r.json(object_hook=JSONObject)
 
@@ -105,30 +149,33 @@ class Cera(object):
 
             for entry in json_dict.response.docs:
                 # write entry_acronym_s into jblob_file
-                values = [entry.entry_acronym_s,
-                          entry.model_s,
-                          entry.qc_experiment_s,
-                          # entry.topic_name_ss,
-                          entry.variable_s,
-                          entry.frequency_s,
-                          entry.entry_name_s,
-                          entry.project_acronym_ss,
-                          entry.format_acronym_s]
-                results_dict[entry.entry_acronym_s.replace(' ', '.')] = entry
+                values = [
+                    entry.entry_acronym_s,
+                    entry.model_s,
+                    entry.qc_experiment_s,
+                    # entry.topic_name_ss,
+                    entry.variable_s,
+                    entry.frequency_s,
+                    entry.entry_name_s,
+                    entry.project_acronym_ss,
+                    entry.format_acronym_s,
+                ]
+                results_dict[entry.entry_acronym_s.replace(" ", ".")] = entry
 
             start += rows
             if start > entries_found:
                 break
 
         query_obj = CeraQuery(self, results_dict)
-        print("{} results found\n".format(len(results_dict)))
+        print(f"{len(results_dict)} results found\n")
 
         return query_obj
 
 
-class CeraQuery(object):
+class CeraQuery:
     """This returns an instance similar to the intake-esm collection class. It provides a search() method as  well as an
-    interfaces to Pandas to view the results as a pd.DataFrame"""
+    interfaces to Pandas to view the results as a pd.DataFrame
+    """
 
     def __init__(self, parent, results_dict):
         """Takes dictionary with lists as values as well as a reference to the parent object (an instance of Cera)"""
@@ -137,14 +184,15 @@ class CeraQuery(object):
 
     def __str__(self):
         self.__repr__()
-        return ''
+        return ""
 
     def __repr__(self):
         from IPython.display import display
-        display(pd.DataFrame(self.df.nunique(), columns=['nunique']))
-        return ''
 
-    def to_jblob(self, download_path, file=None, jblob_log_dir='/tmp/'):
+        display(pd.DataFrame(self.df.nunique(), columns=["nunique"]))
+        return ""
+
+    def to_jblob(self, download_path, file=None, jblob_log_dir="/tmp/"):
         """
         Creates a shell script which downloads assets from CERA by using Jblob.
 
@@ -160,45 +208,45 @@ class CeraQuery(object):
         self.parent.check_access()
 
         if not file:
-            file = os.path.expanduser('~/jblob_cera_download.sh')
+            file = os.path.expanduser("~/jblob_cera_download.sh")
         else:
             file = os.path.expanduser(file)
 
         download_path = os.path.expanduser(download_path)
         os.makedirs(download_path, exist_ok=True)
 
-        jblob_path = shutil.which('jblob')
+        jblob_path = shutil.which("jblob")
         if jblob_path:
             jblob_install_dir = os.path.dirname(jblob_path)
-            jblob_log_dir = os.path.join(jblob_install_dir, 'logs')
+            jblob_log_dir = os.path.join(jblob_install_dir, "logs")
             os.makedirs(jblob_log_dir, exist_ok=True)
         else:
-            jblob_install_dir = ''
+            jblob_install_dir = ""
             print(
-                "\nCouldn't find jblob. Make sure jblob is installed and change the JBLOB_HOME value in {} accordingly.".format(
-                    file))
+                f"\nCouldn't find jblob. Make sure jblob is installed and change the JBLOB_HOME value in {file} accordingly."
+            )
 
-        jblob_log_file = os.path.join(jblob_log_dir, '$0.log')
+        jblob_log_file = os.path.join(jblob_log_dir, "$0.log")
 
-        with open(file, 'w') as jblob_file:
+        with open(file, "w") as jblob_file:
             jblob_file.write(
-                """#!/bin/bash
-                
+                f"""#!/bin/bash
+
                 # This script requires a local Jblob installation. Jblob is a java
                 # based tool for data download from the CERA database available from
                 # https://cera-www.dkrz.de/WDCC/ui/cerasearch/info?site=jblob
-                
+
                 # If you experience problems when using this script or have questions please
                 # contact data@dkrz.de.
-                
+
                 # Please prepare your .netrc file to contain the necessary entry for
                 # CERA database access, for more information see "Supplying username and
                 # password" at https://cera-www.dkrz.de/WDCC/ui/cerasearch/info?site=jblob
-                
+
                 # Edit JBLOB_HOME to match your Jblob installation directory if the
                 # 'jblob' executable cannot be found in $PATH.
-                JBLOB_HOME={0:}
-                
+                JBLOB_HOME={jblob_install_dir}
+
                 # Locate jblob executable
                 if [[ -z $JBLOB_HOME ]]; then
                 JBLOB=$(which jblob 2> /dev/null)
@@ -210,62 +258,79 @@ class CeraQuery(object):
                 else
                 JBLOB=$JBLOB_HOME/jblob
                 fi
-                
+
                 # Logfile
-                LOG={1:}\n\n""".format(jblob_install_dir, jblob_log_file))
+                LOG={jblob_log_file}\n\n"""
+            )
 
-            for entry_name, entry_acronym in zip(self.df['entry_name_s'], self.df['entry_acronym_s']):
-                dataset_dir = os.path.join(download_path, *entry_name.split(' '))
-                jblob_file.write('mkdir -p {}\n'.format(dataset_dir))
-                jblob_file.write('$JBLOB --dataset {} --dir {}\n'.format(entry_acronym, dataset_dir))
-                jblob_file.write('echo "{}: $?" >> $LOG\n'.format(entry_acronym))
+            for entry_name, entry_acronym in zip(
+                self.df["entry_name_s"], self.df["entry_acronym_s"]
+            ):
+                dataset_dir = os.path.join(download_path, *entry_name.split(" "))
+                jblob_file.write(f"mkdir -p {dataset_dir}\n")
+                jblob_file.write(
+                    f"$JBLOB --dataset {entry_acronym} --dir {dataset_dir}\n"
+                )
+                jblob_file.write(f'echo "{entry_acronym}: $?" >> $LOG\n')
 
-            jblob_file.write('\necho "' + '-'*40 + '" >> $LOG')
+            jblob_file.write('\necho "' + "-" * 40 + '" >> $LOG')
             jblob_file.write('\necho "All done..." >> $LOG')
             jblob_file.write('\necho `/bin/date "+%Y-%m-%d %H:%M:%S "` >> $LOG')
-            jblob_file.write('\necho "' + '='*40 + '" >> $LOG\n')
+            jblob_file.write('\necho "' + "=" * 40 + '" >> $LOG\n')
 
-            print('\njblob file "{1:}" successfully created in {0:}\n'.format(*os.path.split(jblob_file.name)))
+            print(
+                '\njblob file "{1:}" successfully created in {0:}\n'.format(
+                    *os.path.split(jblob_file.name)
+                )
+            )
 
-            print("For downloading data from the CERA database, run the created jblob file in the shell.\n"
-                  "Since the download might take a while, it is recommended to start a `screen` environment to run it in the background.\n"
-                  "With `screen` one can start a virtual terminal environment. After starting the file in there, you can easily\n"
-                  "detach yourself from the environment by pressing `Ctrl+A, D`. You will notice a '[detached]' message in the terminal.\n"
-                  "To return to the environment simply type `screen -r` or first list your running screen environments with `screen -list` \n"
-                  "if you have multiple running.\n"
-                  "Check `man screen` for further information.\n")
+            print(
+                "For downloading data from the CERA database, run the created jblob file in the shell.\n"
+                "Since the download might take a while, it is recommended to start a `screen` environment to run it in the background.\n"
+                "With `screen` one can start a virtual terminal environment. After starting the file in there, you can easily\n"
+                "detach yourself from the environment by pressing `Ctrl+A, D`. You will notice a '[detached]' message in the terminal.\n"
+                "To return to the environment simply type `screen -r` or first list your running screen environments with `screen -list` \n"
+                "if you have multiple running.\n"
+                "Check `man screen` for further information.\n"
+            )
 
     @property
     def df(self):
         """Returns a pd.DataFrame of"""
         df_dict = dict()
         for key, entry in self.__results_dict.items():
-            values = [entry.entry_acronym_s,
-                      entry.model_s,
-                      entry.qc_experiment_s,
-                      entry.variable_s,
-                      entry.frequency_s,
-                      entry.entry_name_s,
-                      entry.project_acronym_ss[0],
-                      entry.format_acronym_s]
+            values = [
+                entry.entry_acronym_s,
+                entry.model_s,
+                entry.qc_experiment_s,
+                entry.variable_s,
+                entry.frequency_s,
+                entry.entry_name_s,
+                entry.project_acronym_ss[0],
+                entry.format_acronym_s,
+            ]
             df_dict[key] = values
 
-        columns = ['entry_acronym_s',
-                   'model_s',
-                   'qc_experiment_s',
-                   'variable_s',
-                   'frequency_s',
-                   'entry_name_s',
-                   'project_acronym_ss',
-                   'format_acronym_s']
-        df = pd.DataFrame.from_dict(df_dict, orient='index', columns=columns)
+        columns = [
+            "entry_acronym_s",
+            "model_s",
+            "qc_experiment_s",
+            "variable_s",
+            "frequency_s",
+            "entry_name_s",
+            "project_acronym_ss",
+            "format_acronym_s",
+        ]
+        df = pd.DataFrame.from_dict(df_dict, orient="index", columns=columns)
 
         return df
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     cera = Cera()
     # SEARCH_PATTERN = dict(q="cmip5*historicalNat *mon*tas")
-    cs = cera.search(variable_s='tas', model_s='ACCESS1-0', qc_experiment_s='historical')
+    cs = cera.search(
+        variable_s="tas", model_s="ACCESS1-0", qc_experiment_s="historical"
+    )
     # cs.to_jblob('~/work/cmip5_download')
     print(cs)
--- tests/test_cera.py
+++ tests/test_cera.py
@@ -7,10 +7,12 @@
 # Date:   17/06/2020
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
-import pytest
-from dkrz_cera import Cera, CeraQuery
-import pandas as pd
 import os
+
+import pandas as pd
+import pytest
+
+from dkrz_cera import Cera, CeraQuery
 
 
 @pytest.fixture
@@ -20,7 +22,9 @@ def cera():
 
 @pytest.fixture
 def cera_query(cera):
-    return cera.search(variable_s='tas', model_s='ACCESS1-0', qc_experiment_s='historical')
+    return cera.search(
+        variable_s="tas", model_s="ACCESS1-0", qc_experiment_s="historical"
+    )
 
 
 def test_cera(cera):
@@ -35,7 +39,101 @@ def test_cera_query(cera_query):
 
 
 def test_cera_to_jblob(cera_query, tmpdir):
-    jblob_file = tmpdir.join('jblob_pytest_file.sh')
-    cera_query.to_jblob(tmpdir.join('/donwloads/'), jblob_file)
+    jblob_file = tmpdir.join("jblob_pytest_file.sh")
+    cera_query.to_jblob(tmpdir.join("/donwloads/"), jblob_file)
     assert os.path.exists(jblob_file) == True
     os.remove(jblob_file)
+
+
+# --- Credential source unit tests ---
+
+
+def _get_creds(cera_instance):
+    """Access name-mangled private attributes for test assertions."""
+    return cera_instance._Cera__username, cera_instance._Cera__password
+
+
+def _clear_cera_env(monkeypatch):
+    monkeypatch.delenv("CERA_USER", raising=False)
+    monkeypatch.delenv("CERA_PASSWORD", raising=False)
+    monkeypatch.delenv("CERA_PASS", raising=False)
+
+
+def test_credentials_from_env_vars_password(monkeypatch):
+    """CERA_USER + CERA_PASSWORD env vars are the highest-priority source."""
+    monkeypatch.setenv("CERA_USER", "envuser")
+    monkeypatch.setenv("CERA_PASSWORD", "envpass")
+    monkeypatch.delenv("CERA_PASS", raising=False)
+
+    cera = Cera()
+    cera.check_access()
+    assert _get_creds(cera) == ("envuser", "envpass")
+
+
+def test_credentials_from_env_vars_pass_alias(monkeypatch):
+    """CERA_PASS is accepted as an alias for CERA_PASSWORD."""
+    monkeypatch.setenv("CERA_USER", "envuser")
+    monkeypatch.delenv("CERA_PASSWORD", raising=False)
+    monkeypatch.setenv("CERA_PASS", "aliaspass")
+
+    cera = Cera()
+    cera.check_access()
+    assert _get_creds(cera) == ("envuser", "aliaspass")
+
+
+def test_credentials_from_dotenv_cwd(monkeypatch, tmp_path):
+    """A .env file in the current working directory is used when env vars are absent."""
+    _clear_cera_env(monkeypatch)
+
+    cwd_dir = tmp_path / "cwd"
+    cwd_dir.mkdir()
+    (cwd_dir / ".env").write_text("CERA_USER=cwduser\nCERA_PASSWORD=cwdpass\n")
+
+    # Point HOME somewhere empty so the home .env cannot interfere.
+    fake_home = tmp_path / "home"
+    fake_home.mkdir()
+    monkeypatch.setenv("HOME", str(fake_home))
+    monkeypatch.chdir(cwd_dir)
+
+    cera = Cera()
+    cera.check_access()
+    assert _get_creds(cera) == ("cwduser", "cwdpass")
+
+
+def test_credentials_from_dotenv_home(monkeypatch, tmp_path):
+    """A .env file in the home directory is used when cwd has no .env."""
+    _clear_cera_env(monkeypatch)
+
+    fake_home = tmp_path / "home"
+    fake_home.mkdir()
+    (fake_home / ".env").write_text("CERA_USER=homeuser\nCERA_PASSWORD=homepass\n")
+    monkeypatch.setenv("HOME", str(fake_home))
+
+    # cwd has no .env
+    cwd_dir = tmp_path / "cwd"
+    cwd_dir.mkdir()
+    monkeypatch.chdir(cwd_dir)
+
+    cera = Cera()
+    cera.check_access()
+    assert _get_creds(cera) == ("homeuser", "homepass")
+
+
+def test_credentials_from_netrc(monkeypatch, tmp_path):
+    """~/.netrc is used as the last fallback when no env vars or .env files are present."""
+    _clear_cera_env(monkeypatch)
+
+    fake_home = tmp_path / "home"
+    fake_home.mkdir()
+    (fake_home / ".netrc").write_text(
+        "machine cera-www.dkrz.de login netrcuser password netrcpass\n"
+    )
+    monkeypatch.setenv("HOME", str(fake_home))
+
+    cwd_dir = tmp_path / "cwd"
+    cwd_dir.mkdir()
+    monkeypatch.chdir(cwd_dir)
+
+    cera = Cera()
+    cera.check_access()
+    assert _get_creds(cera) == ("netrcuser", "netrcpass")
